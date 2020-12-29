cd3b119db8aa737588e5323b944f94b5d022a1b8
mls-361 <mls-361@protonmail.com>
2020-12-29 14:32:40 +0100

    En cours de développement

--- go.mod
+++ go.mod
@@ -4,6 +4,6 @@ go 1.15
 
 require (
 	github.com/mls-361/application v0.0.0-20201227165116-6b04370702e3
-	github.com/mls-361/component v0.0.0-20201228223506-c2b84b49889b
+	github.com/mls-361/component v0.0.0-20201229133000-48bc4cf1994c
 	github.com/mls-361/failure v0.0.0-20201227154939-339a9d0e6076
 )
--- internal/components/runner/runner.go
+++ internal/components/runner/runner.go
@@ -8,7 +8,6 @@ package runner
 
 import (
 	"github.com/mls-361/component"
-	"github.com/mls-361/failure"
 )
 
 type (
@@ -31,100 +30,15 @@ func (cr *Runner) Build(_ *component.Manager) error {
 	return nil
 }
 
-func (cr *Runner) initializeComponents(m *component.Manager) error {
-	for _, c := range m.Components() {
-		if c.IsBuilt() {
-			continue
-		}
-
-		if err := c.Initialize(m); err != nil {
-			return failure.New(err).Set("component", c.Name()).Msg("initialization error") /////////////////////////////
-		}
-	}
-
-	return nil
-}
-
-func (cr *Runner) closeComponents(_ *component.Manager) {
-}
-
-func (cr *Runner) recursiveBuild(m *component.Manager, snitch map[string]bool, c component.Component) error {
-	//fmt.Println(c.Name(), "TODO")
-	snitch[c.Category()] = false
-
-	for _, cc := range c.Dependencies() {
-		d, err := m.GetComponent(cc, true)
-		if err != nil {
-			return err
-		}
-
-		if d.IsBuilt() {
-			continue
-		}
-
-		done, ok := snitch[cc]
-		if ok {
-			if done {
-				continue
-			}
-
-			return failure.New(nil).
-				Set("component1", c.Name()).
-				Set("component2", d.Name()).
-				Msg("these two components are interdependent") /////////////////////////////////////////////////////////
-		}
-
-		if err := cr.recursiveBuild(m, snitch, d); err != nil {
-			return err
-		}
-	}
-
-	if err := c.Build(m); err != nil {
-		return failure.New(err).Set("component", c.Name()).Msg("build error") //////////////////////////////////////////
-	}
-
-	//fmt.Println(c.Name(), "DONE")
-	snitch[c.Category()] = true
-
-	return nil
-}
-
-func (cr *Runner) buildComponents(m *component.Manager) error {
-	snitch := make(map[string]bool)
-
-	for _, c := range m.Components() {
-		if c.IsBuilt() {
-			continue
-		}
-
-		done, ok := snitch[c.Category()]
-		if ok {
-			if !done {
-				return failure.New(nil).
-					Set("component", c.Name()).
-					Msg("this error should not occur") /////////////////////////////////////////////////////////////////
-			}
-
-			continue
-		}
-
-		if err := cr.recursiveBuild(m, snitch, c); err != nil {
-			return err
-		}
-	}
-
-	return nil
-}
-
 // Run AFAIRE.
 func (cr *Runner) Run(m *component.Manager) error {
-	if err := cr.initializeComponents(m); err != nil {
+	if err := m.InitializeComponents(); err != nil {
 		return err
 	}
 
-	defer cr.closeComponents(m)
+	defer m.CloseComponents()
 
-	if err := cr.buildComponents(m); err != nil {
+	if err := m.BuildComponents(); err != nil {
 		return err
 	}
 
