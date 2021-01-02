abbaeeccb34a10b211b656a0c3db963f95666fa7
mls-361 <mls-361@protonmail.com>
2021-01-02 15:42:05 +0100

    En cours de développement

--- internal/components/backend/memory/memory.go
+++ internal/components/backend/memory/memory.go
@@ -7,21 +7,30 @@
 package memory
 
 import (
+	"sync"
 	"time"
 
 	"github.com/mls-361/armen/internal/components"
 )
 
 type (
+	lock struct {
+		owner  string
+		expiry time.Time
+	}
+
 	// Backend AFAIRE.
 	Backend struct {
 		components *components.Components
+		mutex      sync.Mutex
+		locks      map[string]*lock
 	}
 )
 
 func New(components *components.Components) *Backend {
 	return &Backend{
 		components: components,
+		locks:      make(map[string]*lock),
 	}
 }
 
@@ -32,11 +41,48 @@ func (cb *Backend) Build() error {
 
 // AcquireLock AFAIRE.
 func (cb *Backend) AcquireLock(name, owner string, duration time.Duration) (bool, error) {
-	return true, nil
+	cb.mutex.Lock()
+	defer cb.mutex.Unlock()
+
+	l, ok := cb.locks[name]
+	if !ok {
+		cb.locks[name] = &lock{
+			owner:  owner,
+			expiry: time.Now().Add(duration),
+		}
+
+		return true, nil
+	}
+
+	if l.owner == owner {
+		l.expiry = time.Now().Add(duration)
+		return true, nil
+	}
+
+	if l.expiry.Before(time.Now()) {
+		l.owner = owner
+		l.expiry = time.Now().Add(duration)
+
+		return true, nil
+	}
+
+	return false, nil
 }
 
 // ReleaseLock AFAIRE.
 func (cb *Backend) ReleaseLock(name, owner string) error {
+	cb.mutex.Lock()
+	defer cb.mutex.Unlock()
+
+	l, ok := cb.locks[name]
+	if !ok {
+		return nil
+	}
+
+	if l.owner == owner {
+		l.owner = ""
+	}
+
 	return nil
 }
 
